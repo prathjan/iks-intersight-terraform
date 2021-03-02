56dd0ecbb87346932040735b27e6527f78b117b7
prathjan <prathjan@cisco.com>
2021-03-02 14:38:40 -0800

    add vars

--- variables.tf
+++ variables.tf
@@ -3,7 +3,9 @@ variable "api_private_key" {
   type = string 
 }
 
-variable "api_key_id" {}
+variable "api_key_id" {
+  type = string 
+}
 
 variable "api_endpoint" {
   default = "https://www.intersight.com"
